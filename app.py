8e0f88a7b980e3c97d15fd2f594db4ce92316f29
Envidia23 <143672343+Envidia23@users.noreply.github.com>
2023-09-02 07:46:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,10 @@ gahsg
 import geopandas as gpd
 Hola
 ···probando esta monda
+colaborando mas 
+seguir colaborando pa tener sipote grafica de kkilo
+colaborar aun mas
+sigo colaborando 
+mas colaboracion
+colaboracion extra 
+colaborando por la vida
